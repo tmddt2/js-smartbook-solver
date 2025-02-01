58d6006c2a778c3b83723a61da635622bdb069ac
tmddt2 <drew.pham@myds.us>
2025-01-31 23:15:48 -0800

    Smartbook 10

--- shortened.js
+++ shortened.js
@@ -1,4 +1,4 @@
-var courseId = ""; /* Put the id of your course here (See README.md for instructions on how to find yours!!) */
+var courseId = "cf3ce96e-01d5-4dad-a6cb-f9596a0b5219"; /* Put the id of your course here (See README.md for instructions on how to find yours!!) */
 fetch("https://raw.githubusercontent.com/rainbrot/js-smartbook-solver/refs/heads/main/solver.js").then(result => result.text()).then((solverCode) => {
     eval(solverCode.replace('var courseId = "', 'var courseId = "' + courseId));
 });
